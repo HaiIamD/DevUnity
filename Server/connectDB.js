9d3b719d3663f70e5b1f64dc9212b2b8ab3f0ed6
Dimas Toriq Sibarani <95232841+HaiIamD@users.noreply.github.com>
2024-04-08 16:44:27 +0700

    Update

--- Server/connectDB.js
+++ Server/connectDB.js
@@ -1,14 +1,1 @@
-const e = require('express');
-const mongoose = require('mongoose');
 
-const ConnectDB = async () => {
-  try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Already Connect to Database`);
-  } catch (error) {
-    console.log(`${error} did not connect`);
-  }
-};
-
-module.exports = ConnectDB;
